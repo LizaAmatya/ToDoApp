f434af1566f9e77f327214ba282aaf167db68898
Liza <lizaamatya@gmail.com>
2020-01-08 14:18:59 +0545

    Updates

---
+++ precache-manifest.f268232dea699988173bdd4509cc8dfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa93ced23e93768077b89c1b26ca701d",
+    "url": "/ToDoApp/index.html"
+  },
+  {
+    "revision": "471e5f888c2340d0e54b",
+    "url": "/ToDoApp/static/css/main.0b36ed1b.chunk.css"
+  },
+  {
+    "revision": "5f1e21f9ff44db701452",
+    "url": "/ToDoApp/static/js/2.4f02760b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ToDoApp/static/js/2.4f02760b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "471e5f888c2340d0e54b",
+    "url": "/ToDoApp/static/js/main.4e1ce76d.chunk.js"
+  },
+  {
+    "revision": "fcd0b651c34dae5d3d83",
+    "url": "/ToDoApp/static/js/runtime-main.23639e4f.js"
+  }
+]);
